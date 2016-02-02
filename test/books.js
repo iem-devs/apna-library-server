39e38345348669c92713b93b4f840f1d0d0356e5
Koustuv Sinha <koustuvsinha@hotmail.com>
2016-02-02 22:08:31 +0530

    Fix #3, also fix build issue

--- test/books.js
+++ test/books.js
@@ -7,8 +7,24 @@
 var request = require('supertest')
 var app = require('../app')
 var expect = require('chai').expect
+var Book = require('../models/Book')
 
 describe('Apna Library Testing suite', function () {
+  var book_id
+
+  before(function (done) {
+    // save one test book in database
+    var testBook = new Book({
+      name: 'Test Book',
+      isbn: 1234
+    })
+    testBook.save(function (err, book) {
+      if (err) console.log('MongoDB save error')
+      book_id = book.id
+      done()
+    })
+  })
+
   // tests whether the api endpoint is accessible or not
   it('should return books api', function (done) {
     request(app)
@@ -31,5 +47,13 @@ describe('Apna Library Testing suite', function () {
         expect(books).to.be.an('array')
         done()
       })
+  })
+
+  after(function (done) {
+    // remove our dummy book entry from database
+    var query = Book.where({_id: book_id})
+    query.find().remove(function () {
+      done()
+    })
   })
 })
